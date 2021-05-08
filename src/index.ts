0d052bf39cd09cca589fae55446c66762bd053c0
John Theodorakopoulos <ioan.theodo@gmail.com>
2021-05-08 17:24:25 +0300

    feat: send jwt on successful (mock) login

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express, { Request, Response } from "express";
 const logger = require("./log/config");
+import jwt from "jsonwebtoken";
 
 const app = express();
 const PORT = 8000;
+
+// will be moved to the env variable
+const maxAge = 60 * 60 * 24 * 365;
 
 function generateRefreshToken(): string {
   return "magic-value";
@@ -15,14 +19,34 @@ function generateAccessToken(): string {
 app.use(logger.successHandler);
 app.use(logger.errorHandler);
 
-app.post("/login", (req: Request, res: Response) => {
+app.post("/login", async (req: Request, res: Response) => {
   res.cookie("session", generateRefreshToken(), {
     httpOnly: true,
-    maxAge: 60 * 60 * 24 * 365,
+    maxAge: maxAge,
   });
 
+  const token: string = await jwt.sign(
+    {
+      user: "john",
+    },
+    "secret-not-safe-at-all",
+    {
+      algorithm: "HS512",
+      expiresIn: maxAge,
+    }
+  );
+
+  const decodedToken = await jwt.decode(token, { complete: true });
+
+  if (!decodedToken) {
+    return res.status(422).json({
+      message: "Invalid JWT token",
+    });
+  }
+
   res.json({
-    jwtToken: generateAccessToken(),
+    jwtToken: decodedToken,
+    jwtExpiryDate: decodedToken.payload.exp,
   });
 });
 
